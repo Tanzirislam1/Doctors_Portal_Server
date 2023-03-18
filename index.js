12226fa935f0986453600048959ab0186f29854e
Tanzirislam1 <aymantanzir@gmail.com>
2023-03-18 18:48:45 +0600

    latest all codes

--- index.js
+++ index.js
@@ -27,6 +27,43 @@ function verifyJWT(req, res, next) {
   });
 }
 
+/* sendgrid-Email_Sender_process: for every individual user after authenticate send Email form server */
+/* at first install noadmaile npm install --save @sendgrid/mail then require the noadmailer */
+/* const emailSenderOptions = {
+  auth: {
+    api_key: process.env.EMAIL_SENDER_KEY
+  }
+}
+
+const emailClient = nodemailer.createTransport(sgTransport(emailSenderOptions));
+
+function sendAppointmentEmail(booking) {
+  const { patient, patientName, treatment, date, slot } = booking;
+  const email = {
+    form: process.env.EMAIL_SENDER,
+    to: patient,
+    subject: `your appointment for ${treatment} is on ${date} at ${slot} is confirmed`,
+    text: `your appointment for ${treatment} is on ${date} at ${slot} is confirmed`,
+    html: `
+      <div>
+        <h1>hello ${patientName}</h1>
+        <p>your appointment for ${treatment} is confirmed</p>
+        <p>Looking forward to seeing you on ${date} at ${slot}</p>
+      </div>
+    `
+  };
+
+  emailClient.sendMail(email, function (err, info) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      console.log('Message Sent', info);
+    }
+  })
+
+} */
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wnmu5yt.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 /* client.connect(err => {
@@ -43,11 +80,24 @@ async function run() {
     console.log('database connected');
     const servicesCollection = client.db('doctors_portal').collection('services');
     const bookingCollection = client.db('doctors_portal').collection('bookings');
-    const userCollection = client.db('doctors_portal').collection('users')
+    const userCollection = client.db('doctors_portal').collection('users');
+    const doctorsCollection = client.db('doctors_portal').collection('doctors');
+
+    const verifyAdmin = async (req, res, next) => {
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({ email: requester });
+      if (requesterAccount.role === 'admin') {
+        next();
+      }
+      else {
+        res.status(403).send({ message: 'Forbidden' });
+      }
+    }
 
     app.get('/service', async (req, res) => {
       const query = {};
-      const cursor = servicesCollection.find(query);
+      /* Project Fields to Return from Query = amra query theke kono data bad diye specific akta data select korar jonne project use kora hoy amra shudhu name ta nite chai slots gulo bad dite chai tai amra  object er moddhe name select korse default valu 0 thake 1 set kora mne project er kaj korano */
+      const cursor = servicesCollection.find(query).project({ name: 1 });
       const service = await cursor.toArray();
       res.send(service);
     });
@@ -68,23 +118,31 @@ async function run() {
     });
 
     /* Admin */
-    app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+    app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
       const email = req.params.email;
+      /* this work are shifted on verifyAdmin function */
       /* admin level role => jokhon user request ashatese tokhon amra user verify kortase then amra database theke user ta k find kortase then user er role jodi admin hoy tahole amra admin filter kortase er jodi user role jodi admin na hoy tahole frobidden status set kortase */
-      const requester = req.decoded.email;
-      const requesterAccount = await userCollection.findOne({ email: requester });
+      // const requester = req.decoded.email;
+      // const requesterAccount = await userCollection.findOne({ email: requester });
       /* if requester role are matched on admin then we are filter the admin access else we get error 403 status */
-      if (requesterAccount.role === 'admin') {
-        const filter = { email: email };
-        const updateDoc = {
-          $set: { role: 'admin' }
-        };
-        const result = await userCollection.updateOne(filter, updateDoc);
-        res.send(result);
-      }
-      else {
-        res.status(403).send({ message: 'forbidden' });
-      }
+      // if (requesterAccount.role === 'admin') {
+      //   const filter = { email: email };
+      //   const updateDoc = {
+      //     $set: { role: 'admin' }
+      //   };
+      //   const result = await userCollection.updateOne(filter, updateDoc);
+      //   res.send(result);
+      // }
+      // else {
+      //   res.status(403).send({ message: 'forbidden' });
+      // }
+
+      const filter = { email: email };
+      const updateDoc = {
+        $set: { role: 'admin' }
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
 
     });
 
@@ -170,9 +228,28 @@ async function run() {
       if (exists) {
         return res.send({ success: false, booking: exists })
       }
+      // sendAppointmentEmail(booking);
       const result = await bookingCollection.insertOne(booking);
       res.send({ success: true, result });
-    })
+    });
+
+    app.post('/doctor', verifyJWT, verifyAdmin, async (req, res) => {
+      const doctor = req.body;
+      const result = await doctorsCollection.insertOne(doctor);
+      res.send(result);
+    });
+
+    app.get('/doctor', verifyJWT, verifyAdmin, async (req, res) => {
+      const doctors = await doctorsCollection.find().toArray();
+      res.send(doctors);
+    });
+
+    app.delete('/doctor/:email', verifyJWT, verifyAdmin, async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const result = await doctorsCollection.deleteOne(filter);
+      res.send(result);
+    });
 
   }
 
